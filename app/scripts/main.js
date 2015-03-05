cfb08ec9a3e028289a23222da0cf436afc0e91be
tshrestha <tshrestha@lab49.com>
2015-03-05 03:59:22 -0500

    prefetching rows

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,29 +1,36 @@
 /* jshint devel:true */
 
-/** @const **/
-var WINDOW = 10;
-/** @const **/
-var VIEWPORT_HEIGHT = 500;
-/** @const **/
-var ROW_HEIGHT = 50;
-/** @const **/
-var SCROLLING = _.uniqueId('SCROLLING');
-/** @const **/
-var NOT_SCROLLING = _.uniqueId('SCROLLING');
-
 
 var Container = Backbone.View.extend({
   getHeightByIndex: function (index) {
     return ROW_HEIGHT;
   },
-  initialize: function(){
+  /*
+    Expect ROW_HEIGHT, RENDER_DELAY and WING_RATIO to be specified
+  */
+  initialize: function(options){
+      this.cfg = {
+        VIEWPORT_HEIGHT : parseInt(this.$el.css('height'), 10),
+        ROW_HEIGHT : options.ROW_HEIGHT || 50,
+        RENDER_DELAY : options.RENDER_DELAY || 500
+      };
+      this.cfg.WINDOW_COUNT = Math.ceil(this.cfg.VIEWPORT_HEIGHT / this.cfg.ROW_HEIGHT) + 1;
+      this.cfg.WING_COUNT = Math.ceil((options.WING_RATIO || 0.25) * this.cfg.WINDOW_COUNT);
+      console.log('cfg', this.cfg);
       this._contentPromises = {};
+      var originalRender = this.render.bind(this, true);
+      var debouncedRender = _.debounce(this.render.bind(this, false), this.cfg.RENDER_DELAY);
+      this.render = function () {
+        originalRender();
+        debouncedRender();
+      }.bind(this);
   },
   tileTemplate: _.template('<person data-index="<%- index %>" class="loading"></person>'),
   tileContentTemplate: _.template('<%- name %> (<%- age %>) <address><%- address %></address>'),
-  getContent: function (index) {
+  getContent: function (index, tag) {
     var promise = this._contentPromises[index];
     if(!promise) {
+      console.debug(tag || 'fetching row#', index);
       var future = $.Deferred();
       promise = future.promise();
       this._contentPromises[index] = future.promise();
@@ -32,16 +39,24 @@ var Container = Backbone.View.extend({
           index: index,
           age: 16 + (( 2 * index ) % 20),
           name: 'Person#' + _.string.pad(index, 2, '0'),
-          address: 'Addres for\n person#' + index
+          address: 'Address for\n person#' + index
         });
       }, 1000);
     }
     return promise;
   },
-  _render : function(scrolling){
-    var from = Math.floor(this.$el.scrollTop() / ROW_HEIGHT);
-    var till = _.min([from + WINDOW, this.model.get('count')]);
-    this.$el.html(_.string.sprintf('<prefix style="display: block; height: %dpx;"> </prefix>', from * ROW_HEIGHT));
+  render : function(scrolling) {
+    var from = Math.floor(this.$el.scrollTop() / this.cfg.ROW_HEIGHT);
+    var till = _.min([from + this.cfg.WINDOW_COUNT, this.model.get('count')]);
+    this.$el.html(_.string.sprintf('<prefix style="display: block; height: %dpx;"> </prefix>', from * this.cfg.ROW_HEIGHT));
+    if(!scrolling) {
+      _.range(_.max([0, from - this.cfg.WING_COUNT]), from).forEach(function (i) {
+        this.getContent(i, 'pre-fetching prefix row#');
+      }.bind(this));
+      _.range(till, _.min([till + this.cfg.WING_COUNT, this.model.get('count')])).forEach(function (i) {
+        this.getContent(i, 'pre-fetching suffix row#');
+      }.bind(this));
+    }
     _.range(from, till).forEach(function (i){
       var $tile = $(this.tileTemplate({index: i})).appendTo(this.$el);
       if (i == 0) {
@@ -50,30 +65,24 @@ var Container = Backbone.View.extend({
       if (i == this.model.get('count') - 1) {
         $tile.addClass('last');
       }
-      var preFetched = this._contentPromises[i] && this._contentPromises[i].state() === 'resolved';
-      if (scrolling === NOT_SCROLLING || preFetched) {
+      var resolved = this._contentPromises[i] && this._contentPromises[i].state() === 'resolved';
+      if (! scrolling || resolved) {
         this.getContent(i).then(function (content){
+          console.debug('Rendering actual row#', i);
           this.$(_.string.sprintf('person[data-index=%d]', i)).html(this.tileContentTemplate(content)).removeClass('loading');
         }.bind(this));
+      } else {
+          console.debug('Rendering placeholder row#', i);
       }
     }.bind(this));
     this.$el.append(_.string.sprintf('<suffix style="display: block; height: %dpx;"> </suffix>',
-      (this.model.get('count') - till) * ROW_HEIGHT));
-  },
-  render: function (){
-    this._render(NOT_SCROLLING);
+      (this.model.get('count') - till) * this.cfg.ROW_HEIGHT));
   }
 });
 
 var container = new Container({
   events: {
-    'scroll': function (){
-      this._render(SCROLLING);
-      if(!this._debouncedRender) {
-        this._debouncedRender = _.debounce(this.render.bind(this, NOT_SCROLLING), 100);
-      }
-      this._debouncedRender();
-    },
+    'scroll': 'render',
     'click person:not(.loading)' : function (event){
       var $tile = $(event.target).closest('person');
       var personId = $tile.data('id');
@@ -87,7 +96,7 @@ var container = new Container({
   },
   el: $('person-list'),
   model: new Backbone.Model({
-    count: 15
+    count: 150
   })
 });
 
