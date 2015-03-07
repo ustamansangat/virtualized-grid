27d495b60971f02afbe8452e1ca0088a76b4fc18
tshrestha <tshrestha@lab49.com>
2015-03-06 23:14:38 -0500

    Expanding should NOT be the responsibility of the generic container

--- app/scripts/VirtualizedContainerTrait.js
+++ app/scripts/VirtualizedContainerTrait.js
@@ -1,64 +1,47 @@
 /* jshint devel:true */
-var  rowTemplate = _.template('<div class="item-row loading" data-index="<%- index %>"></div>');
-
 /* @const */
 var LAZY = _.uniqueId("LAZY");
 
+mixinVirtualizedContainerTrait.LAZY = LAZY;
+
 function mixinVirtualizedContainerTrait (self, options) {
+
   self.events = self.events || {};
+  if (self.events.scroll) {
+    throw new Error("Scroll event handler Note expected");
+  }
   self.events.scroll = 'render';
-  /*{
-    'scroll': 'render',
-    'click .item-row:not(.loading)' : function (event){
-      var $tile = $(event.target).closest('.item-row');
-      var index = $tile.data('index');
-      var expanded = $tile.attr('expanded');
-      if(expanded !== undefined) {
-        $tile.removeAttr('expanded');
-      }  else {
-        $tile.attr('expanded', '');
-      }
-    }
-  },*/
 
   if (!_.isFunction(self.getContent)) {
     throw new Error('Method getContent() is not implemented');
   }
-  if (!_.isFunction(self.rowInternalTemplate)) {
-    throw new Error('Method rowInternalTemplate() is not implemented');
+  if (!_.isFunction(self.rowTemplate)) {
+    throw new Error('Method rowTemplate() is not implemented');
   }
 
   /*
     Expect RENDER_DELAY, MAX_CACHE_COUNT and WING_COUNT to be specified
   */
   var cfg = {
-    VIEWPORT_HEIGHT: parseInt(self.$el.css('height'), 10),
-    RENDER_DELAY: options.RENDER_DELAY || 500,
+    RENDER_DELAY: options.RENDER_DELAY || 100,
     MAX_CACHE_COUNT: options.MAX_CACHE_COUNT || Infinity,
-    WING_COUNT: options.WING_COUNT || 5,
+    WING_COUNT: options.WING_COUNT || 0,
+    VIEWPORT_HEIGHT: parseInt(self.$el.css('height'), 10),
     ROW_HEIGHT: LAZY,
     WINDOW_COUNT: LAZY
   };
 
-  var expandedRows = {};
   var contentPromises = {};
 
   if (self.render !== Backbone.View.prototype.render) {
     throw new Error("No render implementation expected to be provided");
   }
-  self.render = function() {
-    renderOnce();
-    renderAlways();
-  }
+  self.render = _.debounce(render, cfg.RENDER_DELAY);
 
-  self.getIrregularRows = self.getIrregularRows || _.constant({});
+  self.isIrregularRow = self.isIrregularRow || _.constant(false);
 
-  function fingRowByIndex(index) {
+  function findRowByIndex(index) {
     return self.$(_.string.sprintf('.item-row[data-index=%d]', index));
-  }
-
-  function isIrregularOrExpandedRow(i) {
-    return _.has(self.getIrregularRows(), i) || _.has(expandedRows, i);
   }
 
   function validateDimensions($row){
@@ -68,51 +51,74 @@ function mixinVirtualizedContainerTrait (self, options) {
     if (parseInt($row.css('margin-top'), 10) || parseInt($row.css('margin-bottom'), 10)) {
       throw new Error('vertical margins should be 0');
     }
-    if ($row.css('box-sizing') === 'border-box') {
-      if (parseInt($row.css('height'), 10) !== cfg.ROW_HEIGHT) {
-        throw new Error(_.string.sprintf('Height for row#%d is expected to be %d but found %d.', cfg.ROW_HEIGHT, $row.css('height')));
-      }
-    } else {
+    if ($row.css('box-sizing') !== 'border-box') {
       throw new Error('Only border-box supported.');
+    }
+    if(!self.isIrregularRow($row.data('index'))) {
+      var actualHeight = parseInt($row.css('height'), 10);
+      if (actualHeight !== cfg.ROW_HEIGHT) {
+        throw new Error(_.string.sprintf('Height for row#%d is expected to be %d but found %d.', cfg.ROW_HEIGHT, actualHeight));
+      }
     }
   }
 
-  var renderOnce = _.once(function () {
-    self.$el.empty();
-    for (var i = 0; i < self.model.attributes.count && i !== cfg.WINDOW_COUNT && cfg.ROW_HEIGHT === LAZY; ++i) {
-      var $row = $(rowTemplate({index: i})).appendTo(self.$el);
-      if (!isIrregularOrExpandedRow(i) && cfg.ROW_HEIGHT === LAZY) {
-        cfg.ROW_HEIGHT = parseInt($row.css('height'), 10);
-        cfg.WINDOW_COUNT = Math.ceil(cfg.VIEWPORT_HEIGHT / cfg.ROW_HEIGHT) + 1;
-        console.log('cfg', cfg);
-        if (cfg.MAX_CACHE_COUNT < cfg.WINDOW_COUNT + 2 * cfg.WING_COUNT) {
-          throw new Error(_.string.sprintf('We should cache more than the WINDOW + TWICE the WING (%d + 2 * %d)',
-            cfg.WINDOW_COUNT, cfg.WING_COUNT));
-        }
-        validateDimensions($row, cfg.ROW_HEIGHT);
+  function renderRow(i) {
+    return getCachedContent(i).then(function (content) {
+      var $row = $(self.rowTemplate(content)).addClass('item-row').attr('data-index', i);
+      if (i === 0) {
+        $row.addClass('item-row-first');
       }
-    }
-    self.$el.empty();
-    return renderAlways();
-  });
+      if (i === self.model.attributes.count - 1) {
+        $row.addClass('item-row-last');
+      }
+      return $row;
+    });
+  }
 
-  var renderAlways = _.debounce(function () {
+  function setupDimensions () {
+    self.$el.empty();
+    for (var i = 0; i < self.model.attributes.count; ++i) {
+      if (!self.isIrregularRow(i)) break;
+    }
+    return renderRow(i).then(function ($row) {
+      $row.appendTo(self.$el);
+      cfg.ROW_HEIGHT = parseInt($row.css('height'), 10);
+      cfg.WINDOW_COUNT = Math.floor(cfg.VIEWPORT_HEIGHT / cfg.ROW_HEIGHT) + 2; //2 partially visible rows on each end
+      console.log(cfg);
+      if (cfg.MAX_CACHE_COUNT < cfg.WINDOW_COUNT + 2 * cfg.WING_COUNT) {
+        throw new Error(_.string.sprintf('We should cache more than the WINDOW + TWICE the WING (%d + 2 * %d)',
+          cfg.WINDOW_COUNT, cfg.WING_COUNT));
+      }
+      self.$el.empty();
+    });
+  }
+
+  function render() {
+    console.log('SCROLLING ' , arguments);
+    cfg.ROW_HEIGHT === LAZY ? setupDimensions().then(renderHelper) : renderHelper();
+    return self;
+  }
+
+  function renderHelper () {
+    if (self.fetching) {
+      return;
+    }
     /* @const */
     var scrollTop = self.$el.scrollTop();
     /* @const */
     //inclusive
-    var visibleFrom = _.max([0 , Math.floor(scrollTop / cfg.ROW_HEIGHT)]);
+    var visibleFrom = cfg.ROW_HEIGHT === LAZY ? 0 : _.max([0 , Math.floor(scrollTop / cfg.ROW_HEIGHT)]);
     /* @const */
     //exclusive
-    var visibleTill = _.min([visibleFrom + cfg.WINDOW_COUNT, self.model.attributes.count]);
+    var visibleTill = cfg.WINDOW_COUNT === LAZY ? this.model.attributes.count : _.min([visibleFrom + cfg.WINDOW_COUNT, self.model.attributes.count]);
 
     if (visibleFrom === visibleTill) {
       console.log('Nothing is visible');
       return;
     }
-    if (fingRowByIndex(visibleFrom).size() && fingRowByIndex(visibleTill - 1).size()) {
+    if (findRowByIndex(visibleFrom).size() && findRowByIndex(visibleTill - 1).size()) {
       //Already rendered
-      console.debug('Visible rows are already in view');
+      console.debug('Visible rows', visibleFrom, '-', visibleTill - 1,'are already in view');
       return;
     }
 
@@ -120,71 +126,60 @@ function mixinVirtualizedContainerTrait (self, options) {
     var till = _.min([visibleTill + cfg.WING_COUNT, self.model.attributes.count]);
     var range = _.range(from, till);
 
-    self.$el.html(_.string.sprintf('<prefix style="display: block; height: %dpx;"> </prefix>',
-      from * cfg.ROW_HEIGHT));
-    range.forEach(function (i){
-      self.$el.append(rowTemplate({index: i}));
-    });
-    self.$el.append(_.string.sprintf('<suffix style="display: block; height: %dpx;"> </suffix>',
-      (self.model.attributes.count - till) * cfg.ROW_HEIGHT));
-
-    self.$el.scrollTop(scrollTop);
-
-    range.forEach(function (i) {
-      getCachedContent(i).then(function (content){
-        var $row = fingRowByIndex(i);
-        if ($row.size()) {
-          $row.html(self.rowInternalTemplate(content)).removeClass('loading');
-          if (i >= visibleFrom && i < visibleTill && !isIrregularOrExpandedRow (i)) {
-            //for performance reasons we only validate these properties for visible rows.
-            validateDimensions($row);
-          }
-          if (i === 0) {
-            $row.addClass('item-row-first');
-          }
-          if (i === self.model.attributes.count - 1) {
-            $row.addClass('item-row-last');
-          }
-        }
+    $.when.apply($, range.map(renderRow)).then(function () {
+      self.$el.empty();
+      if (cfg.ROW_HEIGHT !== LAZY) {
+        self.$el.html(_.string.sprintf('<prefix style="display: block; height: %dpx;"> </prefix>', from * cfg.ROW_HEIGHT));
+      }
+      Array.prototype.slice.call(arguments).forEach(function($row){
+        self.$el.append($row);
       });
+      if (cfg.ROW_HEIGHT !== LAZY) {
+        self.$el.append(_.string.sprintf('<suffix style="display: block; height: %dpx;"> </suffix>',
+          (self.model.attributes.count - till) * cfg.ROW_HEIGHT));
+      }
+      Array.prototype.slice.call(arguments).forEach(function($row){
+        validateDimensions($row);
+      });
+      console.log("Rendered", from, till - 1);
+      //self.$el.scrollTop(scrollTop);
+      purgeCache(from, till);
     });
-
-
-    return self;
-  }, cfg.RENDER_DELAY);
+  }
 
   function getCachedContent(index) {
     var promise = contentPromises[index];
     if (!promise) {
       promise = contentPromises[index] = self.getContent(index);
-      var population = _.size(contentPromises);
-      if (population > cfg.MAX_CACHE_COUNT) {
-        var firstOne = self.$('.item-row:first').data('index');
-        var lastOne = self.$('.item-row:last').data('index');
-        var i;
-
-        var purgedOnes = []; //for debugging
-        var leftDeletions = 0;
-        for (i = 0; i < firstOne; ++i) {
-          if (_.has(contentPromises, i)) {
-            delete contentPromises[i];
-            purgedOnes.push(i);
-          }
-        }
-
-        var rightDeletions = 0;
-        for (i = self.model.attributes.count - 1; i > lastOne; --i) {
-          if (_.has(contentPromises, i)) {
-            delete contentPromises[i];
-            purgedOnes.push(i);
-          }
-        }
-
-        console.log(_.string.sprintf('Purged %d cached values [%s%s] out of %d, while rendering (%d to %d)',
-          purgedOnes.length, _.string.join(',', _.take(purgedOnes, 5)), purgedOnes.length > 5 ? '...' : '', population, firstOne, lastOne));
-      }
     }
     return promise;
+  }
+
+  function purgeCache(from, till) {
+    var population = _.size(contentPromises);
+    if (population > cfg.MAX_CACHE_COUNT) {
+      var i;
+
+      var purgedOnes = []; //for debugging
+      var leftDeletions = 0;
+      for (i = 0; i < from; ++i) {
+        if (_.has(contentPromises, i)) {
+          delete contentPromises[i];
+          purgedOnes.push(i);
+        }
+      }
+
+      var rightDeletions = 0;
+      for (i = self.model.attributes.count - 1; i > till - 1; --i) {
+        if (_.has(contentPromises, i)) {
+          delete contentPromises[i];
+          purgedOnes.push(i);
+        }
+      }
+
+      console.log(_.string.sprintf('Purged %d cached values [%s%s] out of %d, while rendering (%d to %d)',
+        purgedOnes.length, _.string.join(',', _.take(purgedOnes, 5)), purgedOnes.length > 5 ? '...' : '', population, from, till - 1));
+    }
   }
 
   return self;
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,7 +2,22 @@
 
 var PeopleView = Backbone.View.extend({
 
-  rowInternalTemplate: _.template('<%- name %> <address><%- address %></address>'),
+  events: {
+  'click .item-row:not(.loading)' : function (event){
+      var $row = $(event.target).closest('.item-row');
+      var index = $row.data('index');
+      var expanded = $row.attr('expanded');
+      if(expanded !== undefined) {
+        $row.removeAttr('expanded');
+        delete this._expandedRows[index];
+      } else {
+        $row.attr('expanded', '');
+        this._expandedRows[index] = true;
+      }
+    }
+  },
+
+  rowTemplate: _.template('<div><%- name %> <address><%- address %></address></div>'),
 
   getContent: function (index) {
     var future = $.Deferred();
@@ -11,24 +26,29 @@ var PeopleView = Backbone.View.extend({
         name: 'Person#' + _.string.pad(index, 2, '0'),
         address: 'Address for\n item-row#' + index
       });
-    }, _.random(5) * 200);
+    }, _.random(5) * 100);
     return future.promise();
+  },
+
+  isIrregularRow: function (index){
+    return index === 0 || this._expandedRows[index];
   },
 
   initialize: function (options) {
     Backbone.View.prototype.initialize.apply(this, arguments);
     mixinVirtualizedContainerTrait(this, options);
+    this._expandedRows = {};
   }
 
 });
 
 var container = new PeopleView({
-  MAX_CACHE_COUNT: 70,
-  WING_RATIO: 0.1,
   el: $('people'),
   model: new Backbone.Model({
-    count: 270
+    count: 200000
   }),
+  MAX_CACHE_COUNT: 40,
+  WING_COUNT: 2
 });
 
 container.render();
