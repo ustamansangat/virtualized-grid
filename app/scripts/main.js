21b82e6644b5f68a9be2c56145a9b6f06baacac7
tshrestha <tshrestha@lab49.com>
2015-03-18 20:48:01 -0400

    Separate out ExpandedRow mixin. handle rows with margins propertly (the last row was causing infinite re-rendering)

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,47 +7,72 @@ var _s = require('underscore.string');
 var Backbone = require('backbone');
 
 var mixinVirtualizedContainerTrait = require('../../lib/mixinVirtualizedContainerTrait');
+var CLICK_ROW_EVENT = 'click .item-row';
 
-var PeopleView = Backbone.View.extend({
+/**
+  This implementation assumes that simply marking/un-marking of "expanded"
+  is sufficient. If a the template needs to change in more significant ways,
+  one should NOT use this mixin.
+**/
+var mixinExpandableRows = function (self, options) {
+  var expandedRows = {};
+  var idProperty = options.idProperty || 'id';
 
-  events: {
-    'click .item-row' : function (event){
+  self.events = self.events || {};
+
+  self.events[CLICK_ROW_EVENT] = _.wrap(
+    (typeof self.events[CLICK_ROW_EVENT] === 'string' ? self[CLICK_ROW_EVENT] : self.events[CLICK_ROW_EVENT]) || _.noop,
+    function (clickListener, event){
+      clickListener.apply(self, _.tail(arguments));
       var $row = $(event.target).closest('.item-row');
       var id = $row.data('id');
       var expanded = $row.attr('expanded');
       if(expanded !== undefined) {
         $row.removeAttr('expanded');
-        delete this._expandedRows[id];
-        this.render();
+        delete expandedRows[id];
+        self.render();
       } else {
         $row.attr('expanded', '');
-        this._expandedRows[id] = true;
+        expandedRows[id] = true;
+        //not re-rendering because we assume an expanded row is bigger
       }
+    });
+
+  self.isIrregularRow = _.wrap(self.isIrregularRow, function (isIrregularRow, rowModel) {
+    return isIrregularRow(rowModel) || expandedRows[rowModel.get(idProperty)];
+  });
+
+  self.rowTemplate = _.wrap(self.rowTemplate, function (rowTemplate, rowModel) {
+    var row = rowTemplate.apply(self, _.tail(arguments));
+    if (expandedRows[rowModel.get(idProperty)]) {
+      row = $(row).attr('expanded', '')[0].outerHTML;
     }
-  },
+    return row;
+  });
 
-  idProperty: 'name',
+};
 
-  template: _.template('<div <%= expanded ? "expanded" : ""%> data-id="<%- row.name %>"><%- row.name %> <address><%- row.address %></address></div>'),
+
+var PeopleView = Backbone.View.extend({
+
+  template: _.template('<div data-id="<%- row.name %>"><%- row.name %> <address><%- row.address %></address></div>'),
 
   rowTemplate: function (rowModel) {
     return rowModel.get('heading') ?
       _s.sprintf('<div class="header" style="height:%dpx;">%s</div>', rowModel.get('height'), rowModel.get('heading')) :
       this.template({
-        expanded: this._expandedRows[rowModel.get(this.idProperty)],
         row: rowModel.toJSON()
       });
   },
 
   isIrregularRow: function (rowModel) {
-    return rowModel.get('heading') || this._expandedRows[rowModel.get(this.idProperty)];
+    return rowModel.get('heading');
   },
 
   initialize: function (options) {
     Backbone.View.prototype.initialize.apply(this, arguments);
     mixinVirtualizedContainerTrait(this, options);
-    this._expandedRows = {};
-    this.idProperty = options.idProperty || this.idProperty;
+    mixinExpandableRows(this, options);
   }
 
 });
